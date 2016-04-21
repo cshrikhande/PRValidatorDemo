c541b6c1f6d27af21af2a757546c1a9bee8e66b3
cshrikhande <chshrikh@hotmail.com>
2016-04-21 17:27:50 +0530

    lets check

--- DemoPS.ps1
+++ DemoPS.ps1
@@ -4,3 +4,9 @@ write-host "Hello from my demo ps1"
 ## Contribution from user chshrikh1
 
 write-host "Hello VSTS task world"
+
+#lets check it 
+
+write-host "From valid contribution"
+
+# end of file
